e75ab5721b53f2461737d63c51624747b1db2b46
Dimitrios Metozis <dimitrios.dev@gmail.com>
2018-11-01 14:46:38 +0100

    first commit.

---
+++ SignalRChat/Hubs/IChatClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    public interface IChatClient
+    {
+        Task ReceiveMessage(string user, string message);
+        Task ReceiveMessage(string message);
+    }
+}
